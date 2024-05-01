3052f806717c379cda7eada49844d61a3432f5bc
lulunnqqq <89089631+lulunnqqq@users.noreply.github.com>
2024-05-01 10:05:34 +0700

    chore: Update extracted arrays and decryption key

---
+++ .github/script/getKey.js
@@ -0,0 +1,26 @@
+import fs from 'fs';
+
+
+const getKey = () => {
+
+
+
+    const cleaned = process.argv[2];
+    if (!cleaned) {
+        return;
+    }
+
+    const output = process.argv[3];
+    if (!output) {
+        return;
+    }
+
+    const data =  fs.readFileSync(cleaned, 'utf8');
+    let keyExtracted = data.match(/JScripts\, *\"([^\"]+)/i);
+
+    if (!keyExtracted) {
+        return;
+    }
+    fs.writeFileSync(output, keyExtracted);
+    return;
+}
